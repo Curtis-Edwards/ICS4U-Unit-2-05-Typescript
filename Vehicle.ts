1ad46de075956e106f91a682b231b814f77fcf76
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-08 20:42:08 -0400

    Create Vehicle.ts

---
+++ Vehicle.ts
@@ -0,0 +1,93 @@
+/*
+ * Class that defines string stack.
+ *
+ * @author  Curtis Edwards
+ * @version 1.0
+ * @since   2024-04-05
+ */
+
+export default class Vehicle {
+  // variables
+  private color: string
+  private licencePlate: string
+  readonly numberOfDoors: number
+  readonly maxSpeed: number
+  readonly _speed: number
+
+  /*
+  * Constructor
+  */
+  constructor(color, licencePlate, numberOfDoors, maxSpeed) {
+    this.color = color
+    this.licencePlate = licencePlate
+    this.numberOfDoors = numberOfDoors
+    this.maxSpeed = maxSpeed
+    this._speed = 0
+  }
+
+  /*
+  * This method gets the color
+  */
+  public get color() {
+    return this.color
+  }
+
+  /*
+  * This method gets the licence plate
+  */
+  public get licencePlate() {
+    return this.licencePlate
+  }
+
+  /*
+  * This method gets the current speed
+  */
+  public get speed() {
+    return this._speed
+  }
+
+  /*
+  * This method sets the color
+  */
+  public set color(color) {
+    this.color = color
+  }
+
+  /*
+  * This method sets the licence plate
+  */
+  public set licencePlate(licencePlate) {
+    this.licencePlate = licencePlate
+  }
+
+  /*
+  * This method shows the status of the vehicle
+  */
+  public status() {
+    console.log(" -> Speed: " + this._speed)
+    console.log(" -> Max Speed: " + this.maxSpeed)
+    console.log(" -> Number Of Doors: " + this.numberOfDoors)
+    console.log(" -> Licence Plate: " + this.licencePlate)
+    console.log(" -> Color: " + this.color)
+  }
+
+  /*
+  * This method changes the speed via the accelerating formula
+  */
+  public accelerate (accelerationPower, accelerationTime) {
+    this._speed = (accelerationPower * accelerationTime) + this._speed
+    if (this._speed > this.maxSpeed) {
+      this._speed = this.maxSpeed
+    }
+  }
+
+  /*
+  * This method changes the speed via the braking formula
+  */
+  public break (breakPower, breakTime) {
+    this._speed = this._speed - (breakPower * breakTime)
+    if (this._speed < 0) {
+      this._speed = 0
+    }
+  }
+}
